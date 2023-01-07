0b185839c4f49b2963360dbf8a1f5262aa7e1296
Mateusz Rzeczyca <mateuszrzeczyca17@gmail.com>
2023-01-07 21:23:42 +0100

    added 2 new files, maps and structures

---
+++ maps.py
@@ -0,0 +1,18 @@
+
+# cell_size = 0.1
+# block_size_coefficient = 2.5
+# room_height = 10.0
+# room_width = 20.0
+
+# ASSUMING THOSE PARAMETERS FOR MAPS
+
+CLASS_214_MAP = {
+    "exit_cell":
+    "humans": [
+
+    ],
+    'obstacles': [
+
+    ]
+}
+
