4d3da5d6c6b605b7c5b2d688abe6e550f174520a
Mateusz Rzeczyca <mateuszrzeczyca17@gmail.com>
2023-01-15 17:03:52 +0100

    fixed

--- excel.py
+++ excel.py
@@ -33,10 +33,10 @@ def generate_csv_file_for_simulation(humans_internal_data, occupancy_rate, steps
         worksheet.write(0, 3, 'DISTANCE_TO_PANIC_CELL')
         worksheet.write(0, 4, 'CROWD_DENSITY')
         for index in range(1, len(hid)+1):
-            worksheet.write(index, 0, hid[index]['name'])
-            worksheet.write(index, 1, hid[index]['type'])
-            worksheet.write(index, 2, hid[index]['steps_taken'])
-            worksheet.write(index, 3, hid[index]['distance_to_panic'])
-            worksheet.write(index, 4, hid[index]['crowd_density'])
+            worksheet.write(index, 0, hid[index-1]['name'])
+            worksheet.write(index, 1, hid[index-1]['type'])
+            worksheet.write(index, 2, hid[index-1]['steps_taken'])
+            worksheet.write(index, 3, hid[index-1]['distance_to_panic'])
+            worksheet.write(index, 4, hid[index-1]['crowd_density'])
 
     workbook.close()
